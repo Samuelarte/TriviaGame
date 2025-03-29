984ea0a22df191fe3e0dc2775fc5d5c20387fe24
Samuel Lopez <samuelarte35@gmail.com>
2025-03-28 20:44:07 -0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Erisanny Vargas on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriviaGameTests/TriviaGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameTests.swift
+//  TriviaGameTests
+//
+//  Created by Erisanny Vargas on 3/28/25.
+//
+
+import Testing
+@testable import TriviaGame
+
+struct TriviaGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
